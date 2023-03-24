61de264b1d7a837cdd6ad4c007a38ef228675e6b
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2023-03-24 18:38:02 +0100

    create communication wrapper and settings

---
+++ core/communication/gateway.py
@@ -0,0 +1,47 @@
+import json
+from filip.clients.ngsi_v2.iota import IoTAClient
+from filip.clients.ngsi_v2.cb import ContextBrokerClient
+from filip.clients.ngsi_v2.quantumleap import QuantumLeapClient
+from filip.models.base import FiwareHeader
+from paho.mqtt.client import MQTTv5, Client
+import requests
+from core.settings import settings
+
+
+class Gateway:
+    def __init__(self, *args, **kwargs):
+        # MQTT client
+        self.mqtt_client = Client(protocol=MQTTv5)
+        if settings.MQTT_USER:
+            self.mqtt_client.username_pw_set(username=settings.MQTT_USER, password=settings.MQTT_PASSWORD)
+        if settings.MQTT_TLS:
+            self.mqtt_client.tls_set()
+        self.mqtt_client.connect(host=settings.MQTT_HOST, port=settings.MQTT_PORT)
+
+        # Fiware header
+        # TODO maybe better service name?
+        self.fiware_header = FiwareHeader(service=settings.SCENARIO_NAME, service_path="/")
+        self.scenario_name = settings.SCENARIO_NAME
+
+        # IoTAgent Client
+        s1 = requests.Session()
+        self.iota_client = IoTAClient(url=settings.IOTA_URL, fiware_header=self.fiware_header, session=s1)
+
+        # CB Client
+        s2 = requests.Session()
+        self.cb_client = ContextBrokerClient(url=settings.CB_URL, fiware_header=self.fiware_header, session=s2)
+
+        # QL Client
+        s3 = requests.Session()
+        self.ql_client = QuantumLeapClient(url=settings.QL_URL, fiware_header=self.fiware_header, session=s3)
+
+    def health_check(self):
+        self.mqtt_client.publish(topic="health/check", payload="health check")
+        self.cb_client.get_version()
+        self.iota_client.get_version()
+        self.ql_client.get_version()
+
+
+if __name__ == '__main__':
+    gateway = Gateway()
+    gateway.health_check()
