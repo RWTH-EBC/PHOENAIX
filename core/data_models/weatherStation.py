952d6923a72359facf1ed263e6971e46f32765a6
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2023-06-22 17:57:19 +0200

    chore: add weather station as example

--- core/data_models/weatherStation.py
+++ core/data_models/weatherStation.py
@@ -1,27 +1,62 @@
+import json
+
 from core.data_models import Device
+import pandas as pd
 import time
-import threading
-import random
+from core.data_models import Attribute
 
 
 class WeatherStation(Device):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, weather_data_path, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # setup entity type (fix the entity type)
-        self.entity_type = "WeatherStation"
+        # initialize interactive attributes
+        self.temperature = Attribute(
+            device=self,
+            name="temperature",
+            initial_value=None
+        )
+        self.cloudCover = Attribute(
+            device=self,
+            name="cloudCover",
+            initial_value=None
+        )
+        self.solarDirectRadiation = Attribute(
+            device=self,
+            name="solarDirectRadiation",
+            initial_value=None
+        )
+        self.solarDiffuseRadiation = Attribute(
+            device=self,
+            name="solarDiffuseRadiation",
+            initial_value=None
+        )
+
+        # load weather data
+        weather_data = pd.read_csv(weather_data_path)
+        self.solarDirectRadiation_data = list(weather_data["DirNormRad"].values)
+        self.solarDiffuseRadiation_data = list(weather_data["DiffHorRad"].values)
+        self.cloudCover_data = list(weather_data["TotalSkyCover"].values)
+        self.temperature_data = list(weather_data["DryBulbTemp"].values)
 
     def run(self):
         while True:
             time.sleep(2)
-            self.read_attrs()
-            self.forecast()
-            self.write_attrs()
+            print("read from cloud:")
+            # ONLY FOR DEMONSTRATION
+            # here it does not need to read the latest temperature from FIWARE
+            self.temperature.pull()
 
-    def run_in_thread(self, *args):
-        """Create a new client for the topic"""
-        t = threading.Thread(target=self.run, daemon=True, args=args)
-        t.start()
+            # do your calculation/analysis/etc.
+            self.forecast()
+
+            # send output data to FIWARE
+            # self.write_attrs()
+            print("write to cloud:")
+            self.solarDirectRadiation.push()
+            self.solarDiffuseRadiation.push()
+            self.cloudCover.push()
+            self.temperature.push()
 
     def forecast(self):
         """
@@ -32,25 +67,25 @@ class WeatherStation(Device):
         Returns:
             None
         """
-        # get the input from platform
-        temperature = self.attrs_read["temperature"]
-        solarDirectRadiation = self.attrs_read["solarDirectRadiation"]
-        solarDiffuseRadiation = self.attrs_read["solarDiffuseRadiation"]
-        cloudCover = self.attrs_read["cloudCover"]
+        # ONLY FOR DEMONSTRATION:
+            # how to access the current attribute value
+        temperature = self.temperature.value
 
         # lunch the algorithm
-        temperature += random.uniform(-1, 1)
-        solarDirectRadiation += random.uniform(-1, 1)
-        solarDiffuseRadiation += random.uniform(-1, 1)
-        cloudCover += random.uniform(-1, 1)
-
-        # update the result to attr_write
-        self.attrs_write["temperature"] = temperature
-        self.attrs_write["solarDirectRadiation"] = solarDirectRadiation
-        self.attrs_write["solarDiffuseRadiation"] = solarDiffuseRadiation
-        self.attrs_write["cloudCover"] = cloudCover
+        # assume that here you calculate
+        self.temperature.value = self.temperature_data.pop()
+        self.solarDirectRadiation.value = self.solarDirectRadiation_data.pop()
+        self.solarDiffuseRadiation.value = self.solarDiffuseRadiation_data.pop()
+        self.cloudCover.value = self.cloudCover_data.pop()
 
 
 if __name__ == '__main__':
-    weather_station = WeatherStation(entity_id="WeatherStation:001")
-
+    with open("./schema/WeatherStation.json") as f:
+        data_model = json.load(f)
+    weather_station = WeatherStation(
+        entity_id="WeatherStation:DEQ:MVP:001",
+        entity_type="WeatherStation",
+        data_model=data_model,
+        weather_data_path="D:\Git\deq_demonstrator\\data\\01_input\\01_weather\\DWD_Wetterdaten_Aachen_2018-2020.csv"
+    )
+    weather_station.run()
